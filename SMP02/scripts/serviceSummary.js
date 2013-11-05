3f0a9ceb6ab1c65dd494994654f7f3f6a23a6ddb
Durendal Huynh <durendalhuynh@gmail.com>
2013-11-05 01:51:01 -0600

    Changes made by Durendal Huynh (durendalhuynh@gmail.com).

---
+++ SMP02/scripts/serviceSummary.js
@@ -0,0 +1,30 @@
+(function (global) {
+    var ServiceSummaryViewModel,
+        app = global.app = global.app || {};
+
+    ServiceSummaryViewModel = kendo.data.ObservableObject.extend({
+        serviceSummaryDataSource: null,
+
+        init: function () {
+            var that = this,
+                dataSource;
+
+            kendo.data.ObservableObject.fn.init.apply(that, []);
+
+            dataSource = new kendo.data.DataSource({
+                transport: {
+                    read: {
+                        url: "data/serviceSummary.json",
+                        dataType: "json"
+                    }
+                }
+            });
+
+            that.set("serviceSummaryDataSource", dataSource);
+        }
+    });
+
+    app.serviceSummary = {
+        viewModel: new ServiceSummaryViewModel()
+    };
+})(window);
